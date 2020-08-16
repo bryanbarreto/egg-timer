c70e5cd9989e0a5740e46858f89647935789da7b
bryan barreto <bryan.barreto@icloud.com>
2020-08-16 16:26:16 -0300

    first commit

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var seconds:Int = 0
+    var timer:Timer = Timer()
+    
+   // let eggDic:[String:Int] = ["soft":300,"medium":480,"hard":720]
+    
+        let eggDic:[String:Int] = ["soft":5,"medium":7,"hard":9]
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func startTimer()->Void{
+        
+        timer.invalidate()
+        
+         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self,
+                                      selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+   @objc func updateTimer()->Void{
+    
+    mainLabel.text = String("\(self.seconds) seconds left")
+        
+        if(self.seconds > 0){
+            self.seconds -= 1
+        }else{
+            timer.invalidate()
+            mainLabel.text = "EGG IS DONE!"
+        }
+    }
 
-
+    @IBAction func btnPress(_ sender: UIButton) {
+        mainLabel.text = "Starting Timer..."
+        
+        let hardness:String = sender.currentTitle ?? ""
+        self.seconds = eggDic[hardness]!
+        startTimer()
+    }
+    
 }
 
